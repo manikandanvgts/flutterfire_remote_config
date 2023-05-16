283a2b12946d53b38fe17772f34a3a6f5e49d36b
vgts <abdulla@vishgyana.com>
2023-05-16 07:54:02 +0530

    remote-config web hidded

--- packages/firebase_remote_config/firebase_remote_config_platform_interface/lib/src/method_channel/utils/check_connectivity.dart
+++ packages/firebase_remote_config/firebase_remote_config_platform_interface/lib/src/method_channel/utils/check_connectivity.dart
@@ -3,7 +3,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 abstract class CheckConnectivity {
   CheckConnectivity._();
 
-  Future<bool> isConnected() async {
+  static Future<bool> isConnected() async {
     final connectivityResult = await Connectivity().checkConnectivity();
     return connectivityResult == ConnectivityResult.none;
   }
